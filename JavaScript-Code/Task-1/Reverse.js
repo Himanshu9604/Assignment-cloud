a02a3449e239d492465297d7e3d20eae95d7704a
Himanshu <himanshuyadav201000@gmail.com>
2023-10-16 17:11:39 +0530

    done

--- JavaScript-Code/Task-1/Reverse.js
+++ JavaScript-Code/Task-1/Reverse.js
@@ -1,16 +1,47 @@
 // 1. Take a sentence as an input and reverse every word in that sentence.
 // Example - This is a sunny day > shiT si a ynnus yad.
 
+
 function reverseWords(sentence) {
-    return sentence.split(/\b/).map(word => {
-      if (/[a-zA-Z]/.test(word)) {
-        return word.split('').reverse().join('');
+  const words = [];
+  let currentWord = '';
+  for (let i = 0; i < sentence.length; i++) {
+      const char = sentence[i];
+      if (/[a-zA-Z]/.test(char)) {
+          currentWord = char + currentWord;
+      } else {
+          if (currentWord) {
+              words.push(currentWord);
+              currentWord = '';
+          }
+          words.push(char);
       }
-      return word;
-    }).join('');
   }
+  if (currentWord) {
+      words.push(currentWord);
+  }
+  return words.join('');
+}
+
+const inputSentence = "This is a sunny day!";
+const reversedSentence = reverseWords(inputSentence);
+console.log(reversedSentence);
+
+
+
+/////another way///
+
+
+// function reverseWords(sentence) {
+//     return sentence.split(/\b/).map(word => {
+//       if (/[a-zA-Z]/.test(word)) {
+//         return word.split('').reverse().join('');
+//       }
+//       return word;
+//     }).join('');
+//   }
   
-  const inputSentence = "This is a sunny day!";
-  const reversedSentence = reverseWords(inputSentence);
-  console.log(reversedSentence);
+//   const inputSentence = "This is a sunny day!";
+//   const reversedSentence = reverseWords(inputSentence);
+//   console.log(reversedSentence);
   
--- JavaScript-Code/Task-2/Sorting.js
+++ JavaScript-Code/Task-2/Sorting.js
@@ -1,12 +1,43 @@
 // 2. Perforn sorting of an array in descending order.
 
 
-function sortDescending(arr) {
-    return arr.sort((a, b) => b - a);
-  }  
-  const inputArray = [5, 2, 9, 1, 5, 6];
-  const sortedArray = sortDescending(inputArray);
+
+
+function customSortDescending(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+          if (arr[j] < arr[j + 1]) {
+              
+              const temp = arr[j];
+              arr[j] = arr[j + 1];
+              arr[j + 1] = temp;
+          }
+      }
+  }
+
+  return arr;
+}
+
+const inputArray = [5, 2, 9, 1, 5, 6];
+const sortedArray = customSortDescending(inputArray);
+
+console.log("Original Array:", inputArray);
+console.log("Sorted Array (Descending):", sortedArray);
+    
+
+
+
+
+/// second way ///////
+
+// function sortDescending(arr) {
+//     return arr.sort((a, b) => b - a);
+//   }  
+//   const inputArray = [5, 2, 9, 1, 5, 6];
+//   const sortedArray = sortDescending(inputArray);
   
-  console.log("Original Array:", inputArray);
-  console.log("Sorted Array (Descending):", sortedArray);
+//   console.log("Original Array:", inputArray);
+//   console.log("Sorted Array (Descending):", sortedArray);
   
